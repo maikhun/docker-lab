c9e3f13fe1ad9dc0a6fc3c2cbbd5a528702b5149
andrusyakka <andrusyakka@dopcore.com>
2024-07-04 17:35:04 +0300

    service v.1

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.19
+LABEL authors="andrusyakka@dopcore.com"
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . ./
+
+RUN CGO_ENABLED=0 go build -o ./app
+RUN chmod +x ./app
+
+EXPOSE 8080
+
+CMD ["/app/app"]
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func getRoot(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", getRoot)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
