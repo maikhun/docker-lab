a49bb12d0bca2ec5811fb2382a2a4c47199d2694
Anton Maryushkin <a.maryushkin@bk.ru>
2024-12-19 00:34:05 +0300

    Часть 3

--- main.go
+++ main.go
@@ -1,30 +1,43 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+ "github.com/labstack/echo/v4"
+ "github.com/labstack/echo/v4/middleware"
+ "net/http"
+ "os"
 )
 
-func getRoot(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func getGo(c echo.Context) error {
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		return c.String(http.StatusOK, "Hello, World!\nError:"+err.Error())
+	}
+
+	return c.String(http.StatusOK, "Hello, World!\nHostname:"+hostname)
 }
 
 func getSay(c echo.Context) error {
-	return c.String(http.StatusOK, "Привет, Мир!")
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		return c.String(http.StatusOK, "Привет, мир!\nОшибка:"+err.Error())
+	}
+	
+	return c.String(http.StatusOK, "Привет, мир!\nНаименование хоста:"+hostname)
 }
 
 func main() {
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/", getRoot)
-	e.GET("/say", getSay)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8079"))
+ e := echo.New()
+ 
+ // Middleware
+ e.Use(middleware.Logger())
+ e.Use(middleware.Recover())
+ 
+ // Routes
+ e.GET("/go", getGo)
+ e.GET("/say", getSay)
+ 
+ // Start server
+ e.Logger.Fatal(e.Start(":8079"))
 }
